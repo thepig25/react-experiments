090e57dbd07b0e56ce570226969d5087a1802826
Scott Maclure <scott@maclure.com.au>
2014-01-25 18:42:35 +1100

    Spacing

--- src/react-tutorial/Comment.js
+++ src/react-tutorial/Comment.js
@@ -4,13 +4,18 @@
  * @jsx React.DOM
  */
 var Comment = React.createClass({
+
     render: function () {
+
         var rawMarkup = converter.makeHtml(this.props.children.toString());
+
         return (
             <div className="comment">
                 <div className="comment__author">{ this.props.author }</div>
-                <div className="comment__body" dangerouslySetInnerHTML={{__html: rawMarkup}}/>
+                <div className="comment__text" dangerouslySetInnerHTML={{__html: rawMarkup}}/>
             </div>
         );
+
     }
+
 });
