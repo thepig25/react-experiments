7257d8033fb3d30560bec1378654a604b3ce1c83
Ciaran Hale <ciaran.hale@optus.com.au>
2014-02-04 18:41:30 +1100

    Remove poor pagination.

--- public/app/angular-table/AngularTable.js
+++ public/app/angular-table/AngularTable.js
@@ -2,31 +2,20 @@
 var app = angular.module('angularTableApp', []);
 
 function UserCtrl($scope, $http) {
-	$scope.currentPage = 0;
-    $scope.pageSize = 10;
+	
 	$scope.user = [];	
 	$scope.loadUser = function () {
 		$http({method: 'GET', url: 'data/users-table-angular.json'})
 			.success(function (data, status) {
 				$scope.status = status;
 				$scope.user = data;
-				$scope.numberOfPages = function (){
-		        	return Math.ceil($scope.user.length/$scope.pageSize);                
-			    }
 			})
 			.error(function (data, status) {
 				$scope.data = data || 'Request failed';
 				$scope.status = status;
 			});
 	};	
+
 	$scope.loadUser();
 
 }
-
-//startFrom filter
-app.filter('startFrom', function() {
-    return function(input, start) {
-        start = +start; //parse to int
-        return input.slice(start);
-    }
-});
